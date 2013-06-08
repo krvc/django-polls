24707b7ede71d9ca9513e8d62bfda6b01b9bb2c9
krvc <krvc.it@gmail.com>
2013-06-09 02:38:51 +0530

    Added views for user_page,delete and change

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.utils import timezone
 
@@ -13,7 +14,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published polls."""
-        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        #polls = Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        polls = Poll.objects.filter(pub_date__lte=timezone.now())
+        return polls
 
 
 class DetailView(generic.DetailView):
@@ -32,7 +35,9 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
+@login_required
 def vote(request, poll_id):
+
     p = get_object_or_404(Poll, pk=poll_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
@@ -44,8 +49,48 @@ def vote(request, poll_id):
         })
     else:
         selected_choice.votes += 1
+        selected_choice.voted_user = request.user
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
+        # with POST data. all the votes they gaThis prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+
+@login_required
+def user_page(request):
+    questions = Poll.objects.filter(user=request.user)
+
+    voted_polls = []
+    choices = Choice.objects.filter(voted_user=request.user)
+    for choice in choices:
+        voted_polls.append(Poll.objects.get(id=choice.poll.id))
+    voted_polls = list(set(voted_polls))
+
+    context = {'questions': questions,
+               'voted_polls': voted_polls}
+    return render(request,
+                  'polls/user_page.html',
+                  context)
+
+
+@login_required
+def delete(request, poll_id):
+    poll = Poll.objects.get(id=poll_id)
+    if request.user == poll.user:
+        poll.delete()
+    return HttpResponseRedirect(reverse('polls:user_page'))
+
+
+@login_required
+def change(request, poll_id):
+    if request.method == "GET":
+        poll = Poll.objects.get(id=poll_id)
+        choices = Choice.objects.filter(poll=poll)
+        form = ChangeVotedPollForm(choices)
+        context = {'poll': poll, 'choices': choices, 'form': form}
+        return render(request,
+                      'polls/user_page.html',
+                      context)
+    if request.method == "POST":
+        return HttpResponseRedirect(reverse('polls:user_page'))
